811ab7d833ad5ff87a8ff3be2eba9974128d4c06
Narges Alavi Samani <narges.alavi73@gmail.com>
2020-06-23 10:46:48 -0400

    Directories created

--- src/setup.py
+++ src/setup.py
@@ -1,1 +1,13 @@
-#setup file
+import setuptools
+
+setuptools.setup(
+    name                            = "orquestra",
+    packages                        = setuptools.find_packages(where = "python"),
+    package_dir                     = {
+        "" : "python"
+    },
+    classifiers                     = (
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
